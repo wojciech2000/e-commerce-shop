aa5ad7ed42d871d7b6cfa175618ad63ec2955b5f
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-07-04 10:52:36 +0200

    create delete and update routers

--- routes/Product.js
+++ routes/Product.js
@@ -3,6 +3,7 @@ const passport = require('passport')
 const passportConfig = require('../passport')
 const jwt = require('jsonwebtoken')
 const multer = require('multer')
+const fs = require('fs')
 const Product = require('../models/Product')
 
 const storage = multer.diskStorage({
@@ -25,8 +26,6 @@ const upload = multer({storage, fileFilter})
 
 router.post('/add', passport.authenticate('jwt', {session: false}), upload.single('image'), (req,res) => {
 
-    console.log(req.file)
-
     const product = new Product({
         name: req.body.name,
         price: req.body.price,
@@ -37,9 +36,33 @@ router.post('/add', passport.authenticate('jwt', {session: false}), upload.singl
     })
     product.save(err => {
         if(err)
-        res.status(500).json({message: {msgBody: 'error has occured', msgError: true}})
+            res.status(500).json({message: {msgBody: 'error has occured', msgError: true}})
         else
-        res.status(201).json({message: {msgBody: 'added product', msgError: false}})
+            res.status(201).json({message: {msgBody: product, msgError: false}})
+    })
+})
+
+router.delete('/delete/:id/:filename', passport.authenticate('jwt', {session: false}), (req,res) => {
+    Product.findOneAndDelete({_id: req.params.id}, err => {
+        if(err)
+            res.status(500).json({message: {msgBody: 'error has occured', msgError: true}})
+        else
+            req.params.filename != "undefined" && fs.unlinkSync(`uploads/${req.params.filename}`)
+            res.status(201).json({message: {msgBody: 'removed', msgError: false}})
+    })
+})
+
+router.patch('/update/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), (req,res) => {
+
+    const { name, price, size, brand, quantity } = req.body
+
+    Product.updateOne({_id: req.params.id},
+        {$set: { name, price, size, brand, quantity, image: req.file.filename }},
+        err => {
+        if(err)
+            res.status(500).json({message: {msgBody: 'error has occured', msgError: true}})
+        else
+            res.status(201).json({message: {msgBody: 'updated', msgError: false}})
     })
 })
 
